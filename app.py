ff4ccf223882e1df03fdc820538949391c18c654
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2023-10-26 22:10:57 -0600

    Ajuste de ruta de acceso

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('E:/CARO/Programacion/Tripleten_Data_Scientist/Github/vehicles_us/vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 st.header('Visualización de conjunto de datos de anuncios de venta de coches')
 st.write('Esta aplicación es una práctica')
 
